8cd52abfdb07120d6bc663f54aa0f31acccdf05f
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-12-28 16:43:35 -0300

    implementing game-list component

--- angular/diniz-angular/src/app/app.module.ts
+++ angular/diniz-angular/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GamesListComponent } from './views/home/games-list/games-list.component';
 import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    GamesListComponent
   ],
   imports: [
+    MatTabsModule,
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
